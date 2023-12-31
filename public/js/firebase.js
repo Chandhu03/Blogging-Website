5a7341cfc2a14ef6810486bdb8c4052df50f22a1
chandu nani <chandhumami252@gmail.com>
2023-12-31 18:16:09 +0530

    blogging-website

---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyD4YpN10xFDna8BcyUyEvrUWWKr2gejTPM",
+        authDomain: "blogging-website-21d9f.firebaseapp.com",
+        projectId: "blogging-website-21d9f",
+        storageBucket: "blogging-website-21d9f.appspot.com",
+        messagingSenderId: "806376563040",
+        appId: "1:806376563040:web:c7935b9fa913ced1f3be05"
+      };
+
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
